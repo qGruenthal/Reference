6afe047a7602acb4ff6e2b105f139cea63fa5269
Quinten Cisneros-Gruenthal <quintengruenthal@gmail.com>
2019-06-17 14:52:48 -0700

    Adding Bubble Sort

---
+++ bubble_sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void
+bubble_sort (int* seq, int n)
+{
+  for (int i = 0; i < n - 1; ++i)
+    for (int j = 0; j < n - 1; ++j)
+      if (seq[j] > seq[j + 1])
+	{
+	  int tmp = seq[j];
+	  seq[j] = seq[j + 1];
+	  seq[j + 1] = tmp;
+	}
+}
+  
+
+int
+main ()
+{
+  int seq[] = {3, 5, 2, 4, 1};
+  int n = 5;
+
+  bubble_sort (seq, n);
+
+  for (int i = 0; i < n; ++i)
+    printf ("%d ", seq[i]);
+  printf ("\n");
+}
