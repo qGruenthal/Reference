26b662cdc2daa5688f453ba94500cc6c68621123
Quinten Gruenthal <quintengruenthal@gmail.com>
2019-07-21 11:19:53 -0700

    Adding Todo

---
+++ todo.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo --- TODO -----
+
+if [ $# -eq 1 ]
+then
+  dir=$1
+
+  tmp=$(mktemp)
+  cat $dir/todo | while read task
+  do
+    echo $task > $tmp
+    cat $dir/langs | while read lang
+    do
+      if [ ! -e $dir/$task.$lang ]
+      then
+        echo "  $lang" >> $tmp
+      fi
+    done
+
+    out=`cat $tmp`
+    if [ "$out" != "$task" ]
+    then
+      cat $tmp
+      echo
+    fi
+  done
+
+  rm $tmp
+fi
